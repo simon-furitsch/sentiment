622604e7bf455fac113c73d8692c61eb7e547ae3
Simon Furitsch <accounts@furitsch.net>
2025-09-18 03:32:45 +0200

    ~ Switched to DataFrame and removed a typo

--- src/main/scala/utils/DatasetSplitter.scala
+++ src/main/scala/utils/DatasetSplitter.scala
@@ -2,7 +2,8 @@ package net.furitsch.sentiment
 package utils
 
 import config.ConfigLoader
-import org.apache.spark.sql.Dataset
+
+import org.apache.spark.sql.{DataFrame, Dataset}
 
 object DatasetSplitter {
 
@@ -14,11 +15,10 @@ object DatasetSplitter {
   /**
    * Splits a Dataset into train, validation and test sets
    * Seed & weights set up in the configuration file
-   * @param dataSet dataset to split
-   * @tparam T Type of dataset entries
-   * @return Array(train,valid,test)
+   * @param df dataframe to split
+   * @return Array(train,test,valid)
    * */
-  def split[T](dataSet: Dataset[T]):Array[Dataset[T]] = {
-    dataSet.randomSplit(splitWeights,ConfigLoader.split.seed)
+  def split(df: DataFrame):Array[DataFrame] = {
+    df.randomSplit(splitWeights,ConfigLoader.split.seed)
   }
 }
