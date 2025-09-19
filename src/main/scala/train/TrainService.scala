09a5efa76c9b42fc52a50008fcbd6bf05de6bc21
Simon Furitsch <accounts@furitsch.net>
2025-09-19 06:42:29 +0200

    +Trainservice: train

--- src/main/scala/train/TrainService.scala
+++ src/main/scala/train/TrainService.scala
@@ -1,16 +1,9 @@
 package net.furitsch.sentiment
 package train
 
-import run.{RunContext, RunManager}
 
-import net.furitsch.sentiment.data.DataLoader
-import net.furitsch.sentiment.pipeline.{ClassifierFactory, PipelineBuilder}
-import net.furitsch.sentiment.preprocess.Preprocessor
-import net.furitsch.sentiment.utils.DatasetSplitter
+import pipeline.{ClassifierFactory, PipelineBuilder}
 import org.apache.spark.sql.DataFrame
-import org.apache.spark.sql.classic.SparkSession
-
-import scala.:+
 
 object TrainService {
 
